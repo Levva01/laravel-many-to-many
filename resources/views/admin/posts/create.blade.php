751466290f8e6c296d2ba7bddd29610b2e119374
Levva01 <vellanifederico13@gmail.com>
2022-07-21 14:45:20 +0200

    first commit

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h1>Crea un nuovo post</h1>
+            </div>
+
+            <div class="card-body">
+
+                <form action="{{route('admin.posts.store')}}" method="POST">
+                    @csrf
+                    <div class="form-group">
+                      <label for="title">Titolo</label>
+                      <input type="text" class="form-control @error('title') isinvalid @enderror" id="title" name="title">
+                      @error('title')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                      @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="category">Categoria</label>
+                        <select class="form-control @error('category_id') is-invalid @enderror" id="category" name="category_id">
+                            <option value="">Seleziona Categoria</option>
+                            @foreach ($categories as $category)
+                                <option value="{{$category->id}}" {{old('category_id') == $category->id ? 'selected' : ''}}>{{$category->name}}</option>
+                            @endforeach
+                        </select>
+                        @error('category_id')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <textarea class="ckeditor form-control" name="content"></textarea>
+                    </div>
+                    <div class="form-group form-check">
+                      <input type="checkbox" class="form-check-input" id="published" name="published">
+                      <label class="form-check-label" for="published">Pubblica</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Crea</button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+<script src="//cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('.ckeditor').ckeditor();
+    });
+</script>
